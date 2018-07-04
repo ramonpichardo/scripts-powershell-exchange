fa8b0eb6835dcc5bf06f7fd83016ca52a9a92b19
Ramon <ramonpichardo+github@gmail.com>
2018-07-03 21:51:35 -0700

    Update value syntax for variable $ADUsername

--- owa-get-mailbox-regional-configuration.ps1
+++ owa-get-mailbox-regional-configuration.ps1
@@ -2,6 +2,6 @@
 Trouble accessing OWA? Make sure each of the following four (4) attributes have a value set in the user's Exchange mailbox account: Language, Date Format, Time Format, and Time Zone.
 #>
 
-$ADUsername = <AD username>
+$ADUsername = "<AD_username>"
 
 Get-MailboxRegionalConfiguration -Identity $ADUsername | Format-List 
