ed1414a8fa3d126e136a48f413224e83f2bf64e7
Ramon <ramonpichardo+github@gmail.com>
2018-07-03 21:49:33 -0700

    Update script description

--- owa-get-mailbox-regional-configuration.ps1
+++ owa-get-mailbox-regional-configuration.ps1
@@ -1,5 +1,5 @@
 <#
-Trouble accessing OWA? Make sure the following four (4) parameters are set in the user's Exchange mailbox account: Language, Date Format, Time Format, and Time Zone.
+Trouble accessing OWA? Make sure each of the following four (4) attributes have a value set in the user's Exchange mailbox account: Language, Date Format, Time Format, and Time Zone.
 #>
 
 $ADUsername = <AD username>
